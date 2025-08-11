53379666f116cbfcba0a71e809ce0cde29f04be8
Giulio Gatto <giuliogatto@gmail.com>
2025-08-11 09:35:04 +0100

    Better icons

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,14 +5,24 @@
       <template v-if="authStore.isAuthenticated">
         | <router-link to="/leave_requests">{{ $t('navigation.leaveRequests') }}</router-link>
         | <span class="user-info">{{ $t('common.welcome') }}, {{ authStore.user?.name }}</span>
-        | <NotificationCenter />
-        | <LanguageSwitcher />
-        | <button @click="logout" class="logout-btn">{{ $t('common.logout') }}</button>
+        |
+        <LanguageSwitcher />
+        |
+        <NotificationCenter />
+        <!-- Logout button: using SVG icon -->
+        | <button @click="logout" class="logout-btn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+            stroke-linejoin="round">
+            <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
+            <polyline points="16 17 21 12 16 7" />
+            <line x1="21" y1="12" x2="9" y2="12" />
+          </svg></button>
       </template>
       <template v-else>
         | <router-link to="/login">{{ $t('common.login') }}</router-link>
         | <router-link to="/register">{{ $t('common.register') }}</router-link>
-        | <LanguageSwitcher />
+        |
+        <LanguageSwitcher />
       </template>
     </nav>
     <router-view />
--- frontend/src/components/NotificationCenter.vue
+++ frontend/src/components/NotificationCenter.vue
@@ -2,6 +2,7 @@
   <div class="notification-center">
     <!-- Notification Bell Icon -->
     <div class="notification-bell" @click="toggleNotifications">
+      <!-- Bell icon : using svg icon-->
       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
         <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9"></path>
         <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0"></path>
